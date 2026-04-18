0edd93ff832b7c31d6e2df43d34d3329798b6288
0sinach1 <osimachifeanyi@gmail.com>
2026-04-18 07:35:46 +0100

    Add the java file showcasing multilevel inheritance

--- src/SemTwo/TypesOfInheritance/MultiLevelInheritance.java
+++ src/SemTwo/TypesOfInheritance/MultiLevelInheritance.java
@@ -1,4 +1,22 @@
 package SemTwo.TypesOfInheritance;
+class Vehicle2 {
+    Vehicle2() {
+        System.out.println("This is a Vehicle");
+    }
+}
+class FourWheeler extends Vehicle {
+    FourWheeler() {
+        System.out.println("4 Wheeler Vehicles");
+    }
+}
+class Car2 extends FourWheeler {
+    Car2() {
+        System.out.println("This 4 Wheeler Vehicle is a Car");
+    }
+}
 
 public class MultiLevelInheritance {
+        public static void main(String[] args) {
+            Car2 obj = new Car2(); // Triggers all constructors in order
+        }
 }
