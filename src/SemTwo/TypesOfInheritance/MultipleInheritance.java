bde7a12305ba977d3f89c90a461c8e1f0424a794
0sinach1 <osimachifeanyi@gmail.com>
2026-04-19 13:39:04 +0100

    Add the java class the portrays multiple inheritance with the implement keyword

---
+++ src/SemTwo/TypesOfInheritance/MultipleInheritance.java
@@ -0,0 +1,27 @@
+package SemTwo.TypesOfInheritance;
+interface LandVehicle {
+    default void landInfo() {
+        System.out.println("This is a LandVehicle");
+    }
+}
+interface WaterVehicle {
+    default void waterInfo() {
+        System.out.println("This is a WaterVehicle");
+    }
+}
+// Subclass implementing both interfaces
+class AmphibiousVehicle implements LandVehicle, WaterVehicle {
+    AmphibiousVehicle() {
+        System.out.println("This is an AmphibiousVehicle");
+    }
+}
+public class Test {
+    public static void main(String[] args) {
+        AmphibiousVehicle obj = new AmphibiousVehicle();
+        obj.waterInfo();
+        obj.landInfo();
+    }
+}
+public class MultipleInheritance {
+
+}
